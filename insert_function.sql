ebe301511ca564885c56e7b7b31e917b7a231ff8
anditabinas <38903439+anditabinas@users.noreply.github.com>
2019-08-06 15:39:30 +0800

    Update insert_function.sql

--- insert_function.sql
+++ insert_function.sql
@@ -22,7 +22,7 @@ BEGIN
 
 	--Executes the insert given the supplied geometry, description, and username, while protecting against SQL injection.
     EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, name, notes, address, number, rictmu)
-            VALUES ($1, $2, $3, $4, $5)
+            VALUES ($1, $2, $3, $4, $5, $6)
             ' USING _the_geom, _name, _notes, _address, _number, _rictmu;
             
     RETURN 1;
